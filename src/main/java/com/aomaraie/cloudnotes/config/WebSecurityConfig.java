371b63e2b1cfe8e274f3143f5a2ae595ae5d0930
Ash Omaraie <49348213+aomarai@users.noreply.github.com>
2024-03-08 16:15:57 -0600

    Disabling CSRF to enable POST/PUT/DELETE for now

--- src/main/java/com/aomaraie/cloudnotes/config/WebSecurityConfig.java
+++ src/main/java/com/aomaraie/cloudnotes/config/WebSecurityConfig.java
@@ -8,6 +8,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class WebSecurityConfig {
@@ -15,7 +19,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(Customizer.withDefaults())
+                .csrf().disable()
                 .authorizeRequests(authz -> authz
                         .requestMatchers("/api/**").authenticated()  // Protect API endpoints
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()  // Allow static resources
@@ -27,5 +31,16 @@ public class WebSecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withUsername("user")
+                        .password(passwordEncoder().encode("3dc1d24b-0efd-4881-86f4-3d654f2c7661"))
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 }
